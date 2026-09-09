58bb6f187c708176d42df815848e59c7fbfc8a44
aishwaryamece1-sys <aishwaryam.ece1@gmail.com>
2026-09-09 16:51:14 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Aishwarya-s-Team/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
